37ea4bba164f7f48a2deca6b0e3eda3276e5e433
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-09-30 13:07:40 -0700

    first commit

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+$("document").ready(function(){
+  console.log("inside");
+});
+
+var xhttp;
+if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+    } else {
+    // code for IE6, IE5
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
