5c537749feba76522df2b4747e82aa7442c6ee68
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2016-10-05 09:14:34 -0700

    Morning push for Wednesday

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,8 @@
 "use strict"
-let sourcesArr;
-var sourcesUrl;
-
-// var randomSportSourcesArr = randomElementsArr(sourcesArr);
-// console.log(randomSportSourcesArr);
-
-window.onload = function() {
-  var sportsSection = document.getElementById("sportsSection");
-  sourcesArr = [
-    {
-      source: "BBC Sports",
-      url: "https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=909efd3e931c4e16bde5565b8d10f113"
-    },
+  var sourcesArr = [
     {
       source: "ESPN",
-      url: "https://newsapi.org/v1/articles?source=espn&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
-    },
-    {
-      source: "Football Italia",
-      url: "https://newsapi.org/v1/articles?source=football-italia&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
-    },
-    {
-      source: "FourFourTwo",
-      url: "https://newsapi.org/v1/articles?source=four-four-two&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
+      url: "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=909efd3e931c4e16bde5565b8d10f113"
     },
     {
       source: "Fox Sports",
@@ -37,36 +17,49 @@ window.onload = function() {
       url: "https://newsapi.org/v1/articles?source=sky-sports-news&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
     },
     {
-      source: "talkSport",
-      url: "https://newsapi.org/v1/articles?source=talksport&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
-    },
-    {
-      source: "TheSPORTbible",
-      url: "https://newsapi.org/v1/articles?source=the-sport-bible&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
-    }
-  ];
+    source: "BBC Sports",
+    url: "https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=909efd3e931c4e16bde5565b8d10f113"
+  },
+  {
+    source: "FourFourTwo",
+    url: "https://newsapi.org/v1/articles?source=four-four-two&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
+  },
+  {
+    source: "talkSport",
+    url: "https://newsapi.org/v1/articles?source=talksport&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
+  },
+  {
+    source: "TheSPORTbible",
+    url: "https://newsapi.org/v1/articles?source=the-sport-bible&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
+  },
+  {
+    source: "Football Italia",
+    url: "https://newsapi.org/v1/articles?source=football-italia&sortBy=latest&apiKey=909efd3e931c4e16bde5565b8d10f113"
+  }
+];
+var sourcesUrl;
+// var randomSportSourcesArr = randomElementsArr(sourcesArr);
+// console.log(randomSportSourcesArr);
+
+window.onload = function() {
+  // var randomOrderSources = randomElementsArr(sourcesArr);
+  var sportsSection = document.getElementById("sportsSection");
   for (let i = 0; i < sourcesArr.length; i++) {
     var sourcesUrl = sourcesArr[i].url;
     var sportsArticle = document.createElement("article");
     sportsArticle.className += "sportsArticleClass";
     sourcesArr[i] = sportsArticle;
-    console.log(sourcesUrl);
     apiData(i, sourcesUrl);
-    // sourcesArr[i].addEventListener("load", apiData);
-    //function for adding the content goes here;
-    sportsArticle.textContent = "article area " + i; //remove this when ready
+    // sportsArticle.textContent = "article area " + i; //remove this line when ready
     sportsSection.appendChild(sportsArticle);
   }
 };
 
 //Retrieval of initial data bbc sport
 function apiData(idxForSourcesArr, url) {
-  //Set a different var that returns the url and have it as the var that is going
-  //to render the url in the url key
   console.log("The url is: " + url);
   $.ajax({
     url: url,
-    // url: sourcesArr[idxForSourcesArr]['url'],
     method: "GET",
     success: sportsNewsResult
     // error: bbcNewsError
@@ -74,40 +67,44 @@ function apiData(idxForSourcesArr, url) {
 
   // Inner function in order to close over idxForSourcesArr
   function sportsNewsResult(data) {
-    console.log(data);
     let resultTitle = document.createElement("h3");
+    let resultSource = document.createElement("h4");
     let resultImage = document.createElement("img");
     let resultArticle = document.createElement("p");
+    let resultAnchor = document.createElement("a");
 
     resultTitle.textContent = data.articles[0].title;
+    resultAnchor.setAttribute("href", data.articles[0].url);
+    resultAnchor.setAttribute("target", "_blank");
+    resultAnchor.innerText = "Source: " + data.source + " via newsapi.org ";
+    console.log(data.articles[0].source);
     resultImage.src = data.articles[0].urlToImage;
     resultArticle.textContent = data.articles[0].description;
 
     //
-    console.log(sourcesArr[idxForSourcesArr]);
-    sourcesArr[idxForSourcesArr].appendChild(resultTitle);
-    sourcesArr[idxForSourcesArr].appendChild(resultArticle);
     sourcesArr[idxForSourcesArr].appendChild(resultImage);
+    sourcesArr[idxForSourcesArr].appendChild(resultTitle);
+    resultSource.appendChild(resultAnchor);
+    sourcesArr[idxForSourcesArr].appendChild(resultSource);
+    sourcesArr[idxForSourcesArr].appendChild(resultArticle);
 
   }
-}
-
-
+} //End of apiData function
 
 //Get random elements inside an array;
 function randomElementsArr (array) {
-  let endArray = [];
+  var endArray = [];
   let iterator = array.length;
   for (let i = 0; i < iterator; ) {
     let counter = Math.floor(Math.random() * iterator);
     if (endArray.indexOf(array[counter]) === -1) {
-        endArray.push(array.splice(counter,1));
+      endArray.push(array.splice(counter,1));
     }
     // array.splice(counter,1);
     iterator --;
   }
   return endArray;
-};
+}
 //
 // function bbcNewsError {
 //
